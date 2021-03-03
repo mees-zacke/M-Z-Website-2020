c2fe715a2a3a505b10f23ed940b42ad0f2703bdb
Jerome Wein <florian.krohmer@gmail.com>
2021-03-03 11:10:11 +0100

    removing scrollbar from and repairing leistungen

--- dev/contao/dca/tl_content.php
+++ dev/contao/dca/tl_content.php
@@ -54,30 +54,3 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['mobileSRC'] = [
 
 ];
 
-// Auswählbare Position und Breite von Inhaltselementen
-
-$GLOBALS['TL_DCA']['tl_content']['palettes'] = str_replace(',type',',type,gridStart,gridSpan',$GLOBALS['TL_DCA']['tl_content']['palettes']);
-
-$GLOBALS['TL_DCA']['tl_content']['fields']['gridSpan'] = [
-    'label' => ['Breite', 'Breite des Elements innerhalb des 14er Rasters'],
-    'inputType' => 'select',
-    'options' => ['default', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13' , '14'],
-    'eval' => ['maxlength'=>12, 'tl_class'=>'w50' ],
-    'sql' => [
-        'type' => 'string',
-        'length' => 32,
-        'default' => ''
-    ],
-];
-
-$GLOBALS['TL_DCA']['tl_content']['fields']['gridStart'] = [
-    'label' => ['Position', 'Position des Elements im 12er Raster (Breite und Position dürfen zusammen nicht größer als 15 sein)'],
-    'inputType' => 'select',
-    'options' => ['default', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13' , '14'],
-    'eval' => ['maxlength'=>12, 'tl_class'=>'w50 clr' ],
-    'sql' => [
-        'type' => 'string',
-        'length' => 32,
-        'default' => ''
-    ],
-];
